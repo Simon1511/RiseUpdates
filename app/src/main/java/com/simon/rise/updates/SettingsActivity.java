758c5684bd2f0b1c1a56cf7caacb6c3ce2dfc78d
Simon1511 <simon2002.schoenmackers@gmail.com>
2021-04-24 20:46:52 +0200

    Make the back-arrow in Settings work

--- app/src/main/java/com/simon/rise/updates/SettingsActivity.java
+++ app/src/main/java/com/simon/rise/updates/SettingsActivity.java
@@ -2,7 +2,9 @@ package com.simon.rise.updates;
 
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
@@ -26,6 +28,17 @@ public class SettingsActivity extends AppCompatActivity {
 
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
