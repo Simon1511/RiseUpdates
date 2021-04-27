b85adf86158b586108c80ab37d9b12518775bac4
Simon1511 <simon2002.schoenmackers@gmail.com>
2021-04-27 12:10:00 +0200

    Add another Spinner for download mirror (Page 2)

--- app/src/main/java/com/simon/rise/updates/ui/main/Page2.java
+++ app/src/main/java/com/simon/rise/updates/ui/main/Page2.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Spinner;
@@ -37,6 +38,7 @@ public class Page2 extends Fragment {
     private final HTTPConnecting connect2 = new HTTPConnecting(parser2);
 
     private Spinner spinner1;
+    private Spinner spinner2;
 
     // URLs
     private static final String versionsURL = "https://raw.githubusercontent.com/Simon1511/random/master/versions.json";
@@ -73,6 +75,7 @@ public class Page2 extends Fragment {
 
         getVersions();
         initializeSpinners();
+        onClickSpinners();
 
         checkInstalled();
 
@@ -80,12 +83,12 @@ public class Page2 extends Fragment {
     }
 
     public void getVersions() {
-        // Get kernel types from github JSON
+        // Get Rise-Q versions from Github JSON
         connect.connectURL("rise-q", fragmentView, versionsURL);
     }
 
     public void initializeSpinners() {
-        // Create a dropdown-list for AOSP Q (default), Treble Q and AOSP Pie
+        // Create a dropdown-list for versions
         spinner1 = fragmentView.findViewById(R.id.spinner1_page2);
 
         ArrayAdapter<CharSequence> adapter1 = new ArrayAdapter<>(getContext(), R.layout.spinner_item, parser.getItemList());
@@ -96,6 +99,50 @@ public class Page2 extends Fragment {
         adapter1.add("");
 
         spinner1.setAdapter(adapter1);
+
+        // Create a dropdown-list for download mirrors
+        spinner2 = fragmentView.findViewById(R.id.spinner2_page2);
+
+        ArrayAdapter<CharSequence> adapter2 = new ArrayAdapter<>(getContext(), R.layout.spinner_item, parser2.getItemList());
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        /* Show this as initial item in our spinner.
+        Otherwise, selected items won't show in spinner's preview. */
+        adapter2.add("");
+
+        spinner2.setAdapter(adapter2);
+    }
+
+    public void onClickSpinners()  {
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(spinner1.getSelectedItem().toString().equals("")) {
+                    spinner2.setSelection(0);
+                    spinner2.setEnabled(false);
+                }
+                else
+                if(spinner1.getSelectedItem().toString() != parser.getItemList().toString()) {
+                    spinner2.setSelection(0);
+                    spinner2.setEnabled(true);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
     }
 
     public void checkInstalled() {
