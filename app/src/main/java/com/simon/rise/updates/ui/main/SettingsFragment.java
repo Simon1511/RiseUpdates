6627fa088c75d7cfd71ea15638171da9edbd83c9
Simon1511 <simon2002.schoenmackers@gmail.com>
2021-04-30 19:59:53 +0200

    Add a settings button to choose update interval Also adjust styles to it to have the title of a preference category in orange and its contents in white

--- app/src/main/java/com/simon/rise/updates/ui/main/SettingsFragment.java
+++ app/src/main/java/com/simon/rise/updates/ui/main/SettingsFragment.java
@@ -9,6 +9,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
@@ -26,6 +27,8 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     private static final String versionsURL = "https://raw.githubusercontent.com/Simon1511/random/master/versions.json";
 
     private final AlertDialogRunnable alr = new AlertDialogRunnable();
+
+    private String intervalString = "Daily";
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -50,6 +53,74 @@ public class SettingsFragment extends PreferenceFragmentCompat {
             Toast.makeText(getActivity(), "( ͡° ͜ʖ ͡°)", Toast.LENGTH_SHORT).show();
             return true;
         });
+
+        Preference updateInterval = findPreference("updateIntervalList");
+
+        if(intervalString != null) {
+            updateInterval.setSummary(intervalString);
+        }
+
+        updateInterval.setOnPreferenceClickListener(preference -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(), R.style.PreferenceAlertDialog);
+            AlertDialog alertDialog;
+
+            builder.setTitle("Update check interval");
+
+            builder.setItems(R.array.updateInterval, (dialog, which) -> {
+                if(which == 0) {
+                    intervalString = "Every 12hrs";
+                }
+                else
+                if(which == 1) {
+                    intervalString = "Daily";
+                }
+                else
+                if(which == 2) {
+                    intervalString = "Weekly";
+                }
+                else
+                if(which == 3) {
+                    intervalString = "Never";
+                }
+                else
+                {
+                    intervalString = "None";
+                }
+                updateInterval.setSummary(intervalString);
+            });
+
+            alertDialog = builder.create();
+
+            alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Back", (dialog, which) ->
+                    alertDialog.dismiss());
+
+            alertDialog.show();
+
+            return true;
+        });
+    }
+
+    public long getInterval() {
+        if(intervalString.equals("Every 12hrs")) {
+            return 43200000;
+        }
+        else
+        if(intervalString.equals("Daily")) {
+            return 86400000;
+        }
+        else
+        if(intervalString.equals("Weekly")) {
+            return 604800000;
+        }
+        else
+        if(intervalString.equals("Never")) {
+            return -1;
+        }
+        else
+        {
+            // Default to daily
+            return 86400000;
+        }
     }
 
     @Override
