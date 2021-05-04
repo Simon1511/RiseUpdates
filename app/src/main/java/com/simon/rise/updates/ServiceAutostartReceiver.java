1398e0bbbea56847f3bc7b00562a9d3662b7cf96
Simon1511 <simon2002.schoenmackers@gmail.com>
2021-05-04 20:31:00 +0200

    Make UpdateService start on device boot

---
+++ app/src/main/java/com/simon/rise/updates/ServiceAutostartReceiver.java
@@ -0,0 +1,18 @@
+package com.simon.rise.updates;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class ServiceAutostartReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "ServiceAutostartReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent int1 = new Intent(context, UpdateService.class);
+            context.startForegroundService(int1);
+        Log.i(TAG, "UpdateService started");
+    }
+}
