Simon1511/RiseUpdates

commit c04a99781d456489c6b668a7271210ebd7990ff1

Make spinners and their textView invisible if selection is empty

app/src/main/java/com/simon/rise/updates/ui/main/Page1.java
@@ -201,7 +201,10 @@ public class Page1 extends Fragment {
     }
 
     public void onClickSpinners() {
-        // Disable spinners depending on selection
+        TextView version = fragmentView.findViewById(R.id.textView_chooseVersion_page1);
+        TextView mirror = fragmentView.findViewById(R.id.textView_chooseDownload_page1);
+
+        // Make spinners invisible (GONE) depending on selection
         spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -210,15 +213,19 @@ public class Page1 extends Fragment {
                 if(spinner1.getSelectedItem().toString().equals("")) {
                     Log.i(TAG, "onItemSelected Spinner1: Empty");
                     spinner2.setSelection(0);
-                    spinner2.setEnabled(false);
-                    spinner3.setEnabled(false);
+                    spinner2.setVisibility(View.GONE);
+                    spinner3.setVisibility(View.GONE);
+                    mirror.setVisibility(View.GONE);
+                    version.setVisibility(View.GONE);
+
                     parser3.getItemList().clear();
                 }
                 else
                 {
                     Log.i(TAG, "onItemSelected Spinner1: " + spinner1.getSelectedItem().toString());
                     spinner2.setSelection(0);
-                    spinner2.setEnabled(true);
+                    spinner2.setVisibility(View.VISIBLE);
+                    version.setVisibility(View.VISIBLE);
                 }
 
                 setSpinnerItems();
@@ -238,11 +245,15 @@ public class Page1 extends Fragment {
                 if(spinner2.getSelectedItem().equals("")) {
                     Log.i(TAG, "onItemSelected Spinner2: Empty");
                     spinner3.setSelection(0);
-                    spinner3.setEnabled(false);
+                    spinner3.setVisibility(View.GONE);
+                    mirror.setVisibility(View.GONE);
                 }
 
                 // Get download links for whatever is selected in the spinner
                 if(!spinner2.getSelectedItem().toString().equals("")) {
+                    spinner3.setVisibility(View.VISIBLE);
+                    mirror.setVisibility(View.VISIBLE);
+
                     Log.i(TAG, "onItemSelected: " + spinner2.getSelectedItem().toString());
 
                     // We have different download links for v1.3 (Treble) and v1.3 (AOSP)

app/src/main/java/com/simon/rise/updates/ui/main/Page2.java
@@ -130,16 +130,22 @@ public class Page2 extends Fragment {
     }
 
     public void onClickSpinners()  {
+        TextView mirror = fragmentView.findViewById(R.id.textView_chooseDownload_page2);
+
         spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 if(spinner1.getSelectedItem().equals("")) {
                     Log.i(TAG, "onItemSelected Spinner1: Empty");
                     spinner2.setSelection(0);
-                    spinner2.setEnabled(false);
+                    spinner2.setVisibility(View.GONE);
+                    mirror.setVisibility(View.GONE);
                 }
 
                 if(spinner1.getSelectedItem() != "") {
+                    mirror.setVisibility(View.VISIBLE);
+                    spinner2.setVisibility(View.VISIBLE);
+
                     Log.i(TAG, "onItemSelected Spinner1: " + spinner1.getSelectedItem().toString());
 
                     Log.i(TAG, "onItemSelected Spinner1: Connecting to GitHub");

app/src/main/java/com/simon/rise/updates/ui/main/Page3.java
@@ -13,6 +13,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -123,15 +124,21 @@ public class Page3 extends Fragment {
     }
 
     public void onClickSpinners()  {
+        TextView mirror = fragmentView.findViewById(R.id.textView_chooseDownload_page3);
+
         spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 if(spinner1.getSelectedItem().equals("")) {
                     spinner2.setSelection(0);
-                    spinner2.setEnabled(false);
+                    spinner2.setVisibility(View.GONE);
+                    mirror.setVisibility(View.GONE);
                 }
 
                 if(spinner1.getSelectedItem() != "") {
+                    mirror.setVisibility(View.VISIBLE);
+                    spinner2.setVisibility(View.VISIBLE);
+
                     Log.i(TAG, "onItemSelected Spinner1: " + spinner1.getSelectedItem().toString());
 
                     Log.i(TAG, "onItemSelected Spinner1: Connecting to GitHub");

commit f2c9b8fdefa433a1370a3a8dec7a95c2f1b9ea3a

Fix typos that were accidentally introduced in c04a99781d456489c6b668a7271210ebd7990ff1 Also don't make spinners "GONE", but rather invisible

app/src/main/java/com/simon/rise/updates/ui/main/Page1.java
@@ -208,7 +208,7 @@ public class Page1 extends Fragment {
         TextView version = fragmentView.findViewById(R.id.textView_chooseVersion_page1);
         TextView mirror = fragmentView.findViewById(R.id.textView_chooseDownload_page1);
 
-        // Make spinners invisible (GONE) depending on selection
+        // Make spinners invisible depending on selection
         spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -217,10 +217,11 @@ public class Page1 extends Fragment {
                 if(spinner1.getSelectedItem().toString().equals("")) {
                     Log.i(TAG, "onItemSelected Spinner1: Empty");
                     spinner2.setSelection(0);
-                    spinner2.setVisibility(View.GONE);
-                    spinner3.setVisibility(View.GONE);
-                    mirror.setVisibility(View.GONE);
-                    version.setVisibility(View.GONE);
+                    spinner2.setVisibility(View.INVISIBLE);
+                    spinner3.setVisibility(View.INVISIBLE);
+                    mirror.setVisibility(View.INVISIBLE);
+                    version.setVisibility(View.INVISIBLE);
+                    dlButton.setEnabled(false);
 
                     parser3.getItemList().clear();
                 }
@@ -249,8 +250,9 @@ public class Page1 extends Fragment {
                 if(spinner2.getSelectedItem().equals("")) {
                     Log.i(TAG, "onItemSelected Spinner2: Empty");
                     spinner3.setSelection(0);
-                    spinner3.setVisibility(View.GONE);
-                    mirror.setVisibility(View.GONE);
+                    spinner3.setVisibility(View.INVISIBLE);
+                    mirror.setVisibility(View.INVISIBLE);
+                    dlButton.setEnabled(false);
                 }
 
                 // Get download links for whatever is selected in the spinner

app/src/main/java/com/simon/rise/updates/ui/main/Page2.java
@@ -138,8 +138,9 @@ public class Page2 extends Fragment {
                 if(spinner1.getSelectedItem().equals("")) {
                     Log.i(TAG, "onItemSelected Spinner1: Empty");
                     spinner2.setSelection(0);
-                    spinner2.setVisibility(View.GONE);
-                    mirror.setVisibility(View.GONE);
+                    spinner2.setVisibility(View.INVISIBLE);
+                    mirror.setVisibility(View.INVISIBLE);
+                    dlButton.setEnabled(false);
                 }
 
                 if(spinner1.getSelectedItem() != "") {

app/src/main/java/com/simon/rise/updates/ui/main/Page3.java
@@ -131,8 +131,9 @@ public class Page3 extends Fragment {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 if(spinner1.getSelectedItem().equals("")) {
                     spinner2.setSelection(0);
-                    spinner2.setVisibility(View.GONE);
-                    mirror.setVisibility(View.GONE);
+                    spinner2.setVisibility(View.INVISIBLE);
+                    mirror.setVisibility(View.INVISIBLE);
+                    dlButton.setEnabled(false);
                 }
 
                 if(spinner1.getSelectedItem() != "") {